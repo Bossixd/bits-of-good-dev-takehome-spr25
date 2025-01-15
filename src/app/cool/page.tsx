af997223f7e8fb9bef68d686b5a88185812987fd
Bossixd <bosspattakit123@gmail.com>
2025-01-15 12:56:07 -0500

    added something cool

--- src/app/cool/page.tsx
+++ src/app/cool/page.tsx
@@ -1,13 +1,30 @@
 export default function Kewl() {
-  return (
-    <div className="min-h-screen flex flex-col justify-center items-center bg-primary text-white gap-5">
-      {/* 
+    return (
+        <div className="min-h-screen flex flex-col justify-center items-center bg-primary text-white gap-5">
+            {/* 
       Write something unique about you here! 
       It could be a club you're part of, a weird skill you have, or something special that happened to you.
       Feel free to put links, images, whatever! 
       Don't worry about styling- we aren't grading you on this- it's just to get to know you better! :) 
       */}
-      😎😎😎
-    </div>
-  );
+            <div>
+                Hi, I'm Pattakit, but people call me Boss (It's a common Thai
+                name).
+            </div>
+            <div>
+                I love playing valorant in my free time, but my charger got lost
+                with my luggage while coming here 😔.
+            </div>
+            <div>I also love travelling if I have the chance to.</div>
+            <div>
+                I have been to Yosemite, and I would love to go to the Grand
+                Canyon.
+            </div>
+            <div>
+                I also cook in my free time if I have the ingredients and the
+                time.
+            </div>
+            😎😎😎
+        </div>
+    );
 }
